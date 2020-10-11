a885793cb05d695f1450c5e38628f6089074d35a
furimu1234 <enu7693@gmail.com>
2020-10-11 11:19:48 +0900

    da

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()  
 
 setuptools.setup(  
-    name="echidna",  
+    name="asyncpgw",  
     version="0.0.1",  
-    author="1ntegrale9",  
-    author_email="1ntegrale9uation@gmail.com",  
-    description="BOT and CLI framework for handling multiple services in python",  
+    author="furimu",  
+    description="asyncpg wrapper,  
     long_description=long_description,  
     long_description_content_type="text/markdown",  
-    url="https://github.com/1ntegrale9/echidna",  
+    url="https://github.com/furimu1234/asyncpgw/tree/main/src",  
     packages=setuptools.find_packages(),  
     classifiers=[  
-        "Programming Language :: Python :: 3.7",  
+        "Programming Language :: Python :: 3.8",  
         "License :: OSI Approved :: MIT License",  
         "Operating System :: OS Independent",  
     ],  
