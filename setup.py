a016e39cd9f24ef4b12110f08d8a7cc4ddbe7121
furimu1234 <enu7693@gmail.com>
2020-10-11 11:34:03 +0900

    dafa

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ import setuptools
 with open("README.md", "r") as fh:  
     long_description = fh.read()  
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(  
     name="asyncpgw",  
     version="0.0.1",  
     author="furimu",  
     description="asyncpg wrapper",  
     long_description=long_description,  
-    long_description_content_type="text/markdown",  
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
     url="https://github.com/furimu1234/asyncpgw/tree/main/asyncpgw",  
     packages=setuptools.find_packages(),  
     classifiers=[  
